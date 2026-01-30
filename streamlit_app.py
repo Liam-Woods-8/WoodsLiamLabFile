748fb1a66681f23557ec80c9442c2eab124dbc16
Liam Woods <143724842+Liam-Woods-8@users.noreply.github.com>
2026-01-30 00:28:03 +0000

    small typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-lab1_page = st.Page("Lab1.py", title="lab 1")
-lab2_page = st.Page("Lab2.py", title="lab 2")
+lab1_page = st.Page("lab1.py", title="lab 1")
+lab2_page = st.Page("lab2.py", title="lab 2")
 
 pg = st.navigation([lab2_page, lab1_page])
 
